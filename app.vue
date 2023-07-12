7fd5df0134f01ebc1c3e7eb51a27fdb1da55b15f
kai <1363560774@qq.com>
2023-07-13 00:11:51 +0800

    Initial commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<!--<template>-->
+<!--  <div>-->
+<!--    <NuxtWelcome />-->
+<!--  </div>-->
+<!--</template>-->
+<script setup lang="ts">
+const version = 2 + 1
+</script>
+
+<template>
+  <div class="hello">
+    Hello Nuxt {{ version }}!
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
